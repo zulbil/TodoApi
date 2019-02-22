66b49f75acf4f3b9fce91da301b9bf7506002a83
Joel Alexandre Khang Zulbal <alexkhang25@yahoo.fr>
2019-02-22 17:14:17 +0100

    Adding authenticate middleware to todos routes

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,16 +6,16 @@ var todoCtrl        = require('./../controllers/Todos');
 var {authenticate}  = require('./../middlewares/authenticate'); 
 
 //Todos Routes
-router.post('/new/todo', todoCtrl.todoCreate); 
-router.get('/todos', todoCtrl.todoList); 
-router.get('/todos/:id', todoCtrl.todoById); 
-router.delete('/todos/remove/:id', todoCtrl.todoDelete);
+router.post('/new/todo', authenticate, todoCtrl.todoCreate); 
+router.get('/todos', authenticate, todoCtrl.todoList); 
+router.get('/todos/:id',authenticate, todoCtrl.todoById); 
+router.delete('/todos/remove/:id',authenticate, todoCtrl.todoDelete);
 router.patch('/todos/edit/:id', todoCtrl.todoUpdate); 
 
 
 //Users Routes
-router.post('/new/user', userCtrl.userCreate); 
-router.get('/user/me', authenticate, userCtrl.userProfile); //Private routes
+router.post('/user/signup', userCtrl.userCreate); 
+router.get('/user/profile', authenticate, userCtrl.userProfile); //Private routes
 router.post('/user/login', userCtrl.userLogin); 
 router.delete('/user/logout', authenticate, userCtrl.userLogout);
 
